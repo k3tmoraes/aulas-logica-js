7d1dd6ab7b4d6add0dd823ee719e922156960e47
KETLEN BEATRIZ MORAES FURTADO <ketlen_furtado@estudante.sesisenai.org.br>
2025-04-16 22:19:18 -0300

    feat:variáveis

---
+++ variaveis.js
@@ -0,0 +1,18 @@
+// declaração de variável com var
+
+// var nome = "kit"
+
+//  var nome = "ket"
+
+// declaração de váriavel com let
+
+// let nome ="kit"
+
+// nome = "ket"
+
+// variável declarada com const
+// const nome = "kit"
+
+// nome ="ket"
+
+ console.log(nome)
